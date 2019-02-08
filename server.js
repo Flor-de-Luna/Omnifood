4b5b79b08390808f36b828ec5ccd1af0d295e9f2
Flor-de-Luna <janieehutts@gmail.com>
2019-02-07 18:41:28 -0700

    dynamically register css

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 const sever = app.listen();
-app.use(express.static(__dirname + "/static"));
+const path = require('path');
+
+app.use(express.static(__dirname + "static"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'static'))); //dynamically register css
+
 app.get('/', function(req, res){
     res.render('index')
 });
